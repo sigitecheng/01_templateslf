c665f151cdf9c76d38e7afeadd715ae3d52fa28f
sigitseptiadi <sigitseptiadi1@gmail.com>
2024-11-21 13:39:07 +0700

    d

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('');
+    return view('frontend.halamanutama.index');
 });
 
 Route::get('/dashboard', function () {
